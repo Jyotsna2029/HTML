99da97d27b3b3a95c9ec49b052ac973ce1e298f6
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2025-08-15 22:18:21 +0530

    Add files via upload

---
+++ JS/JavaScript part-5/assign.js
@@ -0,0 +1,24 @@
+// ques1
+let num = Math.floor(Math.random()*6)+1;
+console.log(`number of the dice ${num}`);
+
+// ques 2
+const car = {
+    name :"mercedes",
+    model: "benz",
+    color: "black"
+};
+console.log(car);
+console.log(car.name);
+
+// ques 3
+const person = {
+    name:"Suresh",
+    age: 46, 
+    city: "Jaipur"
+}
+console.log(person);
+person.city = "New York";
+console.log(person);
+person.country = "USA";
+console.log(person);
