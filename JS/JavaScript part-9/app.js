59a8045d5a2c4514c76d1322771d61b140eb8a36
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2025-08-25 19:21:34 +0530

    Add files via upload

--- JS/JavaScript part-9/app.js
+++ JS/JavaScript part-9/app.js
@@ -22,4 +22,49 @@ console.dir(document.querySelector("h1")); //first
 console.dir(document.querySelector("div a")); //first div ke andar anchor tag
 console.dir(document.querySelectorAll("#description")); //all
 
+// manipulating stylee
+let links = document.querySelectorAll('.box a');
+for(let i = 0; i<links.length; i++){
+    links[i].style.color = "yellow";
+}
 
+// classlist
+let heading = document.querySelector('h1');
+heading.classList.add("green");
+heading.classList.add("unline");
+heading.classList.remove("unline");
+heading.classList.contains("green");
+heading.classList.toggle("unline");
+
+// navigation
+let h4 = document.querySelector("h4");
+// parent element
+console.log(h4.parentElement);
+
+// children
+let box = document.querySelector(".box");
+console.log(box.children);
+
+// pervsibling / nextsibling
+let ul = document.querySelector('ul');
+console.log(ul.previousElementSibling);
+console.log(ul.nextElementSibling);
+
+// creating element in para
+let newP  =document.createElement('p');
+newP.innerText("Hi, I am new Paragraph!")
+let body = document.querySelector('body');
+body.appendChild(newP);
+
+//
+let btn = document.createElement('button');
+btn.innerHTML = "NEW BUTTON!!!";
+let p = document.querySelector('p');
+p.insertAdjacentElement('beforebegin', btn);
+p.insertAdjacentElement('afterbegin', btn);
+p.insertAdjacentElement('beforeend', btn);
+p.insertAdjacentElement('afterend', btn);
+
+// remove
+
+btn.remove();
--- JS/JavaScript part-9/apps.js
+++ JS/JavaScript part-9/apps.js
@@ -13,3 +13,25 @@ console.log(para.innerHTML);
 // manipulation
 para.innerText = "Hey, I am peter parker";
 console.log(para);
+
+
+// manipulating attributes
+let img = document.querySelector("img");
+img.getAttributes('id');
+img.setAttribute('id', 'spiderman');
+
+
+// manipulating styles
+img.style;
+img.style.backgroundColor = "green";
+
+let links = document.querySelectorAll('.box a');
+for(let i = 0; i<links.length; i++){
+    links[i].style.color = "yellow";
+}
+
+// classlist manipulation
+let clsList = document.querySelector('img');  //empty vale
+clsList.classList; //null since there is no class
+
+
